2adeda4a86649a4098bf2669fff60a5d62e3e8da
Austin Raney <aaraney@crimson.ua.edu>
2020-12-09 13:18:12 -0500

    add silly code that doesnt do anything

---
+++ pr_demo/core.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def foo(x: int) -> int:
+    """This is just some test function
+
+    Parameters
+    ----------
+    x : int
+        Some silly x
+    """
+    return x
